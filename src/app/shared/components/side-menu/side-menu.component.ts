10eaeeadec3f89056e3ff0f1e23c5493912d8310
Milton Munoz <livemilton@gmail.com>
2025-06-18 23:05:15 -0500

    reactive-forms :sparkles: init project

---
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-side-menu',
+  imports: [RouterOutlet],
+  templateUrl: './side-menu.component.html',
+  
+})
+export class SideMenuComponent { }
