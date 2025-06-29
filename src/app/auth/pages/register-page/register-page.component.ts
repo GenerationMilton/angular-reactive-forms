a769f9d7a2764ac1611fe65096089c46d2004127
Milton Munoz <livemilton@gmail.com>
2025-06-29 11:41:01 -0500

    reactive-forms :sparkles: personalized validation of same fields password and confirm

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,12 @@
 import { JsonPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validator, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  ReactiveFormsModule,
+  Validator,
+  Validators,
+} from '@angular/forms';
 import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
@@ -9,24 +15,38 @@ import { FormUtils } from '../../../utils/form-utils';
   templateUrl: './register-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RegisterPageComponent { 
-
-  fb = inject(FormBuilder)
+export class RegisterPageComponent {
+  fb = inject(FormBuilder);
 
   formUtils = FormUtils;
 
   myForm = this.fb.group({
-    name:['', [Validators.required, Validators.pattern(this.formUtils.namePattern)]],
-    email:['',[Validators.required, Validators.pattern(this.formUtils.emailPattern)]],
-    username:['',[Validators.required, Validators.minLength(6), Validators.pattern(this.formUtils.notOnlySpacesPattern)]],
-    password:['',[Validators.required, Validators.minLength(6)]],
-    password2:['',Validators.required],
-
+    name: [
+      '',
+      [Validators.required, Validators.pattern(this.formUtils.namePattern)],
+    ],
+    email: [
+      '',
+      [Validators.required, Validators.pattern(this.formUtils.emailPattern)],
+    ],
+    username: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern(this.formUtils.notOnlySpacesPattern),
+      ],
+    ],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', Validators.required],
+  },{
+    validators: [
+      this.formUtils.isFieldOneEqualFieldTwo('password','password2')
+    ]
   });
 
-  onSubmit(){
+  onSubmit() {
     this.myForm.markAllAsTouched();
     console.log(this.myForm.value);
   }
-
 }
