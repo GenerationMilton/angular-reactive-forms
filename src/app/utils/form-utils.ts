7ab7515031f480b2a0f92be44516dfedfea695ca
Milton Munoz <livemilton@gmail.com>
2025-06-29 11:25:19 -0500

    reactive-forms :sparkles: validations vs regular expressions

--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -2,9 +2,13 @@ import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
 
 export class FormUtils {
   //Expresiones regulares
-
+  static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+  static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
 
   static getTextError(errors: ValidationErrors){
+
+
 
     for (const key of Object.keys(errors)) {
       switch (key) {
@@ -19,6 +23,15 @@ export class FormUtils {
 
         case 'email':
           return `El valor ingresado no es un correo electrónico`;
+
+        case 'pattern':
+          if(errors['pattern'].requiredPattern === FormUtils.emailPattern){
+            return 'El valor ingresado no luce como un correo electrónico';
+          } 
+          return 'Error de patrón contra expresión regular';
+
+        default:
+          return `Error de validacion no controlado ${key}`;
       }
     }
 
