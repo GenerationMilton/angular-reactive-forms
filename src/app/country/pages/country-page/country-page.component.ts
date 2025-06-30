621108ebf9d8bdcde0a88b3f5b1d647c7d3e42fc
Milton Munoz <livemilton@gmail.com>
2025-06-29 21:12:46 -0500

    reactive-forms :sparkles: first selector regions consume service

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,9 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './country-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CountryPageComponent { }
+export class CountryPageComponent {
+
+  fb = inject(FormBuilder);
+
+  countryService = inject(CountryService);
+  //consume service
+  regions = signal(this.countryService.regions);
+  //consume service
+  countriesByRegion = signal<Country[]>([]);
+  //consume service
+  borders = signal<Country[]>([]);
+
+  //validations of forms
+  myForm = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    border: ['', Validators.required],
+  })
+
+
+ }
